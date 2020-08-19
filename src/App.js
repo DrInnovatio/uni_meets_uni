82fe55d05dcf0f80f6d3457b742c352169aeae89
DrInnovatio <turing1723@gmail.com>
2020-08-19 16:00:27 +1000

    from kokos laptop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
-import LandingPage from '../src/Landing/Landing.jsx';
+/** @format */
 
+import React from "react";
+import "./App.css";
+import LandingPage from "../src/Landing/Landing.jsx";
 
 function App() {
-  return (
-
-    <div >
-      <LandingPage / >
-    </div>
-  );
+	return (
+		<div>
+			<LandingPage />
+		</div>
+	);
 }
 
 export default App;
