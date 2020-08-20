a9f9959ece243a0e193536a40e94daee0441ac2c
DrInnovatio <turing1723@gmail.com>
2020-08-20 22:50:19 +1000

    added a background on the landing page.

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-//import { makeStyles } from '@material-ui/core/styles'
-import backgroundPic from '../images/youngPeople.jpg'
+import { makeStyles } from '@material-ui/core/styles'
 
-function LandingPage() {
-  return (
-    <div styles={ { backgroundImage: `url(${backgroundPic})` } }>
-      <h1>This is red car</h1>
-    </div>
-  )
+
+const backgroundImage = require('../images/youngPeople.jpg');
+
+const divStyle = {
+  width: '100%',
+  height: '100vh',
+  backgroundImage: `url(${backgroundImage})`,
+  backgroundSize: 'cover'
+};
+
+export default class Landing extends React.Component {
+  render() {
+    return (
+      <div style={divStyle} >
+        <h1 align="center">Some header example</h1>
+      </div>
+    );
+  }
 }
-
-export default LandingPage;
