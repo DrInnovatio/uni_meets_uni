989674b42257461aa78b3b630b00fb53e0c4423e
DrInnovatio <turing1723@gmail.com>
2020-08-20 23:55:15 +1000

    put the text banner on the landing page.

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-
 
 const backgroundImage = require('../images/youngPeople.jpg');
 
@@ -8,14 +6,32 @@ const divStyle = {
   width: '100%',
   height: '100vh',
   backgroundImage: `url(${backgroundImage})`,
-  backgroundSize: 'cover'
+  backgroundSize: '100%',
+  position: "absolute"
+};
+
+const bannerText = {
+  background: "pink",
+  boxShadow: "5px 10px #800020",
+  width: "50%",
+  height: "200px",
+  borderRadius: "10px",
+  opacity: "0.8",
+  margin: "27% 25%",
+  position: "fixed",
+  textAlign: "center",
+  paddingTop: ""
 };
 
 export default class Landing extends React.Component {
+  
   render() {
     return (
       <div style={divStyle} >
-        <h1 align="center">Some header example</h1>
+        <div style={bannerText}>
+              <h1>Uni Meets Uni</h1>  
+              <p>The New Way for Socializing</p> 
+        </div>
       </div>
     );
   }
