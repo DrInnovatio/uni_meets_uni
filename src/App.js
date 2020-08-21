4f47072102b894354cd5f7f7f7692d6f2051b622
DrInnovatio <turing1723@gmail.com>
2020-08-21 22:04:51 +1000

    deleted SecondPage.jsx file

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@
 import React from "react";
 import "./App.css";
 import LandingPage from "../src/Landing/Landing.jsx";
-import whyUmU from "../src/Landing/SecondPart.jsx";
+
 
 function App() {
 	return (
 		<div>
 			<LandingPage />
-			<whyUmU/>
 		</div>
 	);
 }
