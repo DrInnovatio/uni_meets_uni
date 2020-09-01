dbb66001cff04b2b2215a967b2ff183bb54bfe9c
DrInnovatio <turing1723@gmail.com>
2020-09-01 21:26:21 +1000

    fixed app.js file.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
-/** @format */
-
-import React from "react";
-import "./App.css";
-import LandingPage from "../src/Landing/Landing.jsx";
-
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from 'react-router-dom'
+import './App.css'
+import Home from './components/pages/Home.jsx'
+import Services from './components/pages/Services.jsx'
+import Products from './components/pages/Products.jsx'
+import SignUp from './components/SignInUp.jsx'
 
 function App() {
-	return (
-		<div>
-			<LandingPage />
-		</div>
-	);
+	return ( <
+		div >
+		<
+		Router >
+		<
+		Navbar / >
+		<
+		Switch >
+		<
+		Route path = '/'
+		exact component = {
+			Home
+		}
+		/> 
+
+		<
+		Route path = '/services'
+		component = {
+			Services
+		}
+		/> 
+
+		<
+		Route path = '/products'
+		component = {
+			Products
+		}
+		/>  <
+		Route path = '/sign-up'
+		component = {
+			SignUp
+		}
+		/>  <
+		/Switch> 
+
+		<
+		Router / >
+
+		<
+		div / >
+	)
 }
 
-export default App;
+export default App
