9b5cbddaee332023bc1b201c4820fad6b4d1cc7f
DrInnovatio <turing1723@gmail.com>
2020-09-11 00:08:37 +1000

    added an explain of the app.

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -6,7 +6,17 @@ import { FaAppStore } from 'react-icons/fa';
 export default function Products() {
   return (
     <div className="products">
-      <h4 >You can download the apps right now</h4>
+      <h2 >You can download the apps right now</h2>
+      <div style={ { textAlign: "center" } }>
+        <h4>Sorry. This application is still beta version,</h4>
+        <h5>and being tested with the students of</h5>
+        <div style={ { fontSize: "25px", margin: "20px", color: "#2ec1ac" } }>
+          <p>Queensland University of Technology</p>
+          <p>University of Queensland</p>
+          <p>Griffith University</p>
+          <p>Central Queensland University</p>
+        </div>
+      </div>
 
       <div className="react-icons">
         <FaGooglePlay />
