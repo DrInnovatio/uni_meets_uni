d8328552e5c92bdd337661e58b7cb608e19bc83b
DrInnovatio <turing1723@gmail.com>
2020-09-10 19:15:13 +1000

    added google and apple icons.

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import '../../App.css'
+import { FaGooglePlay } from 'react-icons/fa';
+import { FaAppStore } from 'react-icons/fa';
 
 export default function Products() {
   return (
     <div className="products">
       <h4 >You can download the apps right now</h4>
 
-      <i className="fa fa-instagram"></i>
-      <i className="fa fa-google-play"></i>
-      <i className="fa fa-apple-alt"></i>
+      <div>
+        <FaGooglePlay />
+        <FaAppStore />
+      </div>
     </div>
   )
 }
